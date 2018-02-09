df02bd0b611d47f096227e0c52af011c00dcc8d7
DESKTOP-VMUPS6B\massa <hmmyfh@gmail.com>
2018-02-09 09:01:40 -0600

    commit QS

--- JavaApplication4/src/javaapplication4/Sortings.java
+++ JavaApplication4/src/javaapplication4/Sortings.java
@@ -68,12 +68,63 @@ public class Sortings {
     } 
     
 
-    private ArrayList<Comparable> QS(){
+    private ArrayList<Comparable> QS(Comparable arr[], int low, int high){
         
-        
+         Comparable pivot = arr[high]; 
+        int i = (low-1); // index of smaller element
+        for (int j=low; j<high; j++)
+        {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j].compareTo(pivot) <= 0 )
+            {
+                i++;
+ 
+                // swap arr[i] and arr[j]
+                Comparable temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+ 
+        // swap arr[i+1] and arr[high] (or pivot)
+        Comparable temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+ 
+        return i+1;
+    }
+ 
+ 
+    /* The main function that implements QuickSort()
+      arr[] --> Array to be sorted,
+      low  --> Starting index,
+      high  --> Ending index */
+   /**
+    * Funcion principal que ordena una lista de objetos que implementan la clase Comparable utilizando QuickSort
+    * @param arr
+    * @param low
+    * @param high
+    * @return
+    */
+    public static Comparable[] sort(Comparable arr[], int low, int high)
+    {
+        if (low < high)
+        {
+            /* pi is partitioning index, arr[pi] is 
+              now at right place */
+            int pi = partition(arr, low, high);
+ 
+            // Recursively sort elements before
+            // partition and after partition
+            sort(arr, low, pi-1);
+            sort(arr, pi+1, high);
+        }
+        return arr;
         return numeros;
     }
-    
+    // Codigo obtenido de: http://www.geeksforgeeks.org/iterative-quick-sort/
+    /*This code is contributed by Rajat Mishra */
     private ArrayList<Comparable> Merge(){
         ArrayList<Comparable> ordenados=new ArrayList<Comparable>();
         {
